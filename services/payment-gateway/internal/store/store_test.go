420db5bfbab844b9703b3e9e3e5ef4281db855c3
jacktantram <jack.tantram@gmail.com>
2021-12-06 19:58:12 +0100

    fixing integration tests and CI

--- services/payment-gateway/internal/store/store_test.go
+++ services/payment-gateway/internal/store/store_test.go
@@ -5,6 +5,7 @@ package store_test
 import (
 	"github.com/jacktantram/payments-api/pkg/driver/v1/postgres"
 	"github.com/jacktantram/payments-api/services/payment-gateway/internal/store"
+	log "github.com/sirupsen/logrus"
 	"os"
 	"testing"
 )
@@ -16,9 +17,11 @@ var (
 func TestMain(m *testing.M) {
 	postgresClient, err := postgres.NewClient("postgres://postgres:postgres@localhost:5432?sslmode=disable", "payment")
 	if err != nil {
-		os.Exit(-1)
+		log.Fatal(err)
 	}
-	postgresClient.Migrate("../../migrations")
+	if err := postgresClient.Migrate("../migrations"); err != nil {
+		log.Fatal(err)
+	}
 	testStore = store.NewStore(postgresClient)
 	exitVal := m.Run()
 	os.Exit(exitVal)
